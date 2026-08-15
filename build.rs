741ae3671cc4b1d0e3d923a4d1f22100c9208c03
Mikachu2333 <linkchou@yandex.com>
2026-08-15 12:42:43 +0800

    fix version info

--- build.rs
+++ build.rs
@@ -1,20 +1,33 @@
 fn main() {
-    println!("cargo:rerun-if-changed=icon.ico");
-    println!("cargo:rerun-if-changed=res/rimage_x86.exe");
-    println!("cargo:rerun-if-changed=res/rimage_x64.exe");
+    #[cfg(windows)]
+    {
+        use winresource::VersionInfo;
+        const VERSION_PRE: u16 = 0;
 
-    if std::env::var("CARGO_CFG_WINDOWS").is_ok() {
-        let version = std::env::var("CARGO_PKG_VERSION").expect("package version is set by Cargo");
-        let mut resource = winresource::WindowsResource::new();
-        resource
-            .set_icon("icon.ico")
-            .set("ProductName", "Rimage GUI")
-            .set("FileDescription", "Rimage image conversion GUI")
-            .set("FileVersion", &version)
-            .set("ProductVersion", &version)
-            .set("LegalCopyright", "Copyright (c) 2026 Mikachu2333");
-        resource
-            .compile()
-            .expect("failed to compile Windows resources");
+        println!("cargo:rerun-if-changed=icon.ico");
+        println!("cargo:rerun-if-changed=res/rimage_x86.exe");
+        println!("cargo:rerun-if-changed=res/rimage_x64.exe");
+
+        let env_u64 =
+            |name: &str| -> u64 { std::env::var(name).unwrap_or_default().parse().unwrap_or(0) };
+        if std::env::var("CARGO_CFG_WINDOWS").is_ok() {
+            let pack = |pre: u16| -> u64 {
+                (env_u64("CARGO_PKG_VERSION_MAJOR") << 48)
+                    | (env_u64("CARGO_PKG_VERSION_MINOR") << 32)
+                    | (env_u64("CARGO_PKG_VERSION_PATCH") << 16)
+                    | u64::from(pre)
+            };
+            let mut resource = winresource::WindowsResource::new();
+            resource
+                .set_icon("icon.ico")
+                .set("ProductName", "Rimage GUI")
+                .set("FileDescription", "Rimage image conversion GUI")
+                .set_version_info(VersionInfo::FILEVERSION, pack(VERSION_PRE))
+                .set_version_info(VersionInfo::PRODUCTVERSION, pack(VERSION_PRE))
+                .set("LegalCopyright", "Copyright (c) 2026 Mikachu2333");
+            resource
+                .compile()
+                .expect("failed to compile Windows resources");
+        }
     }
 }
